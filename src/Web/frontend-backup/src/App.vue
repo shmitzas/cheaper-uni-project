e0fb838c64c0fb18d1a83271cf56f4011d69c410
Paul <p.miescionaitis@gmail.com>
2022-04-01 17:30:41 +0300

    migrated whole website to vue.js

---
+++ src/Web/frontend-backup/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+  <div>
+    <NavBar />
+  </div>
+</template>
+
+<style>
+@import "@../public/styles.css";
+</style>
