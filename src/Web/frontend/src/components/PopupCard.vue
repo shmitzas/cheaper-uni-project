e84b6ba2f8fea67006c03330a0de4fcefed6d2ef
shmitzas <46751666+shmitzas@users.noreply.github.com>
2022-04-13 14:03:00 +0300

    card generation with data population

--- src/Web/frontend/src/components/PopupCard.vue
+++ src/Web/frontend/src/components/PopupCard.vue
@@ -22,7 +22,6 @@
           <div class="row justify-content-center mb-5 mt-5 ms-5 me-5">
             <div class="col-md d-block mb-4">
               <div class="card best-price pb-3">
-                <!-- <div class="popup-prod-best-price"><span>BEST PRICE!</span></div> -->
                 <div class="popup-store-img">
                   <img src="../assets/img/Lidl_logo.png" alt="" />
                 </div>
@@ -43,5 +42,7 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: ['prodName', 'prodOldPrice', 'prodPrice', 'priceDate'],
+};
 </script>
